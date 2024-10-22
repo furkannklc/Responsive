43714982f97c0851c945de6aa699ca16ac1dcdfe
furkannklc <furkannkilic43@gmail.com>
2024-10-23 00:18:16 +0300

    neww

---
+++ dfd.js
@@ -0,0 +1,18 @@
+const menu=document.getElementById("1")
+const icon=document.getElementById("2")
+icon.addEventListener("click",degistir)
+
+function degistir(){
+    if(menu.style.display=="flex"){
+        menu.style.display="none"
+        icon.style.marginTop="0px"
+        
+    }
+    else{
+        menu.style.display="flex"
+        icon.style.marginTop="200px"
+        icon.style.marginRight="0px"
+    }
+   
+
+}
